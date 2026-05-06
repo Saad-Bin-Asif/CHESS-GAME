6063d8462f56e671852efd68ebbeb355878fd1a8
Ahmed Hasaan <ahmedrai4543@gmail.com>
2026-05-06 22:47:45 +0500

    Knight Logic

--- Chess/Knight.h
+++ Chess/Knight.h
@@ -2,7 +2,6 @@
 #include"Piece.h"
 class Knight:public Piece
 {
-	char name;
 public:
 	Knight(char name, bool isWhite, int row, int column) :Piece(isWhite, row, column,name) {}
 	bool isValidMove(int endRow, int endColumn) override{
@@ -12,4 +11,3 @@ public:
 		
 	}
 };
-
