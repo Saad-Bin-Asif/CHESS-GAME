9127e7ea9efa94f966f7730d57c42c5a2190ec54
ahmedrai4543-hub <ahmedrai4543@gmail.com>
2026-05-06 22:53:40 +0500

    Knight

--- Chess/Knight.h
+++ Chess/Knight.h
@@ -2,12 +2,13 @@
 #include"Piece.h"
 class Knight:public Piece
 {
-	
+	char name;
 public:
 	Knight(char name, bool isWhite, int row, int column) :Piece(isWhite, row, column,name) {}
 	bool isValidMove(int endRow, int endColumn) override{
 		if ((abs(position.row - endRow) == 2 && abs(position.column - endColumn) == 1) || (abs(position.row - endRow) == 1 && abs(position.column - endColumn) == 2)) 
 			return true;
+
 		return false;
 		
 	}
