da62fb6ab9b9041ba3c60340f8284684d853cfd2
Muhammad Umer Bajwa <muhammad126umer@gmail.com>
2026-05-06 22:36:48 +0500

    Rook Movement Logic

--- Chess/Rock.h
+++ Chess/Rock.h
@@ -1,17 +1,19 @@
 #pragma once
 #include"Piece.h"
-class Rook:public Piece
+class Rook :public Piece
 {
-	
+
 public:
-	Rook(char name,int isWhite, int row, int column) :Piece(isWhite, row, column,name){}
-	bool isValidMove(int endRow,int endColumn) override {
+	
+	Rook(char name, int isWhite, int row, int column) :Piece(isWhite, row, column, name) {}
+	// moves only in rows and columns
+	bool isValidMove(int endRow, int endColumn) override {
 		if (((position.row == endRow) && (position.column != endColumn)) || ((position.row != endRow) && (position.column == endColumn))) {
-			
-			
+
+
 			return true;
 		}
-			
+
 		return false;
 	}
 	char getName() {
