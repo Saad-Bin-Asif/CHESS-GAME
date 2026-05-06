d6638f7d144df364c400f8c893d279311ddb03ca
Muhammad Umer Bajwa <muhammad126umer@gmail.com>
2026-05-06 21:53:38 +0500

    Bishop movement logic updated

--- Chess/Bishop.h
+++ Chess/Bishop.h
@@ -1,12 +1,12 @@
 #pragma once
 #include"Piece.h"
-class Bishop:public Piece 
+class Bishop :public Piece
 {
-	
+
 public:
-	Bishop(char name, bool isWhite, int row, int column) : Piece(isWhite, row, column,name){}
+	Bishop(char name, bool isWhite, int row, int column) : Piece(isWhite, row, column, name) {}
 	bool isValidMove(int endRow, int endColumn)override {
-		if ((position.row != endRow && position.column != endColumn)&&(abs(endRow-position.row)==abs(endColumn-position.column)))
+		if ((position.row != endRow && position.column != endColumn) && (abs(endRow - position.row) == abs(endColumn - position.column)))
 			return true;
 		return false;
 	}
