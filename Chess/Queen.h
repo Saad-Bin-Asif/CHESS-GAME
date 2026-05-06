d2fb099eb061a16b3fcd0600fb13fb01a2be5c32
Muhammad Umer Bajwa <muhammad126umer@gmail.com>
2026-05-06 22:39:49 +0500

    Queen Movement Logic

--- Chess/Queen.h
+++ Chess/Queen.h
@@ -2,9 +2,10 @@
 #include"Piece.h"
 class Queen:public Piece
 {
-	char name;
+	
 public:
 Queen(char name,bool isWhite,int row,int column):Piece(isWhite,row,column,name){}
+//moves in rows and digonals
 bool isValidMove(int endRow, int endColumn) override {
 	if ((position.row == endRow && position.column != endColumn) || (position.row != endRow && position.column == endColumn)) 
 		return true;
